ecb661e41fb9a5d3d9c715e713f268edc0e0e126
Lee jaewon <ijw1004j@gmail.com>
2023-09-20 12:09:54 +0900

    이미지 자르기, 깊은 복사, 반전

--- opencv_install_test/study1-1.cpp
+++ opencv_install_test/study1-1.cpp
@@ -4,27 +4,18 @@
 using namespace cv;
 using namespace std;
 
-//20191220 이재원 코드 3-8
+//20191220 이재원 
 
 int main() {
 	cout << "Hello OpenCV" << CV_VERSION << endl;
 
-	Mat img1 = imread("dog.bmp");
+	Mat img1 = imread("cat.bmp");
 
-	Mat img2 = img1;
-	Mat img3;
-	img3 = img1;
-
-	Mat img4 = img1.clone();
-	Mat img5;
-	img1.copyTo(img5);
-	img1.setTo(Scalar(0,255,255));
+	//깊은 복사
+	Mat img2 = ~img1(Rect(220, 120, 340, 240)).clone();
 
 	imshow("img1", img1);
 	imshow("img2", img2);
-	imshow("img3", img3);
-	imshow("img4", img4);
-	imshow("img5", img5);
 
 	waitKey(0);
 	destroyAllWindows();
