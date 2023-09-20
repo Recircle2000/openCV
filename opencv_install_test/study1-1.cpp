d97053093877aecfbf9e38c8cb11ce4d23a8d046
Lee jaewon <ijw1004j@gmail.com>
2023-09-20 11:21:00 +0900

    Mat 클래스의 활용1

--- opencv_install_test/study1-1.cpp
+++ opencv_install_test/study1-1.cpp
@@ -9,7 +9,15 @@ using namespace std;
 int main() {
 	cout << "Hello OpenCV" << CV_VERSION << endl;
 
-	Mat img = imread("lenna.bmp");
+	Mat img = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	Mat img2(480, 640, CV_8UC3, Scalar(0, 255, 255));
+	Mat mat1 = Mat::zeros(3, 3,CV_32SC1);
+
+	mat1.create(480, 640,CV_32FC1);
+	mat1.setTo(1.f);
+
+
+
 
 	if (img.empty())
 	{
@@ -17,8 +25,8 @@ int main() {
 		return -1;
 	}
 
-	namedWindow("image");
-	imshow("image", img);
+	namedWindow("image",WINDOW_AUTOSIZE);
+	imshow("image", mat1);
 
 	waitKey(0);
 	return 0;
