56ae44163abcb6a59591d8f234ac5674a546a746
Lee jaewon <ijw1004j@gmail.com>
2023-10-04 12:17:19 +0900

    카메라 입력 반전하고 동영상으로 저장하기

--- opencv_install_test/study.cpp
+++ opencv_install_test/study.cpp
@@ -5,20 +5,31 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	// VideoCapture cap("stopwatch.avi"); 이렇게 하면 비디오 불러오기
+	//VideoCapture cap("stopwatch.avi"); //이렇게 하면 비디오 불러오기
 	VideoCapture cap(0); // 이렇게 하면 카메라 불러오기
 
 	if (!cap.isOpened())
 	{
-		cerr << "카메라 오픈 실패" << endl;
+		cerr << "카메라 or 피디오 오픈 실패" << endl;
 		return 0;
 	}
 
-	cout << "Frame width" << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
-	cout << "Frame height" << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
+	int w = cvRound(cap.get(CAP_PROP_FRAME_WIDTH));
+	int h = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
+	double fps = cap.get(CAP_PROP_FPS);
+
+	int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+	int delay = cvRound(1000 / fps);
+
+	VideoWriter outputVideo("output.avi", fourcc, fps, Size(w, h));
+
+	if (!outputVideo.isOpened())
+	{
+		cout << "file open failed!" << endl;
+		return 0;
+	}
 
 	Mat frame, inversed;
-
 	while (true)
 	{
 		cap >> frame;
@@ -26,19 +37,17 @@ int main() {
 		{
 			break;
 		}
-		//이 사이에 프레임 처리 코드 작성
 		inversed = ~frame;
-		//이 사이에 프레임 처리 코드 작성
+		outputVideo << inversed;
+
 		imshow("frame", frame);
 		imshow("inversed", inversed);
 
-		if (waitKey(24) == 27)
+		if (waitKey(delay) == 27)
 		{
 			break;
 		}
 	}
-
-	waitKey(0);
 	destroyAllWindows();
 	return 0;
 }
