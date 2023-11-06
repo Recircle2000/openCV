c7693724a5989ae23d1333df29340ab7d0bb4478
Recircle2000 <ijw1004j@gmail.com>
2023-11-06 22:27:41 +0900

    영상의 산술 연산

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -5,74 +5,45 @@
 using namespace cv;
 using namespace std;
 
-void on_Contrast(int pos, void* userdata);
-
-Scalar mean();
-
-Mat calcGrayHist(const Mat& img)
+int main(void)
 {
-	CV_Assert(img.type() == CV_8UC1);
+	Mat src1 = imread("lenna256.bmp", IMREAD_GRAYSCALE);
+	Mat src2 = imread("square.bmp", IMREAD_GRAYSCALE);
 
-	Mat hist;
-	int channels[] = { 0 };
-	int dims = 1;
-	const int histSize[] = {256};
-	float graylevel[] = {0, 256};
-	const float* ranges[] = {graylevel};
-
-	calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
-
-	return hist;
-}
-Mat getGrayHistImage(const Mat& hist)
-{
-	CV_Assert(hist.type() == CV_32FC1);
-	CV_Assert(hist.size() == Size(1, 256));
-
-	double histMax;
-	minMaxLoc(hist, 0, &histMax);
-
-	Mat imgHist(100, 256, CV_8UC1, Scalar(255));
-	for (int i = 0; i < 256; i++)
+	if (src1.empty() || src2.empty())
 	{
-		line(imgHist, Point(i, 100),
-			Point(i, 100 - cvRound(hist.at<float>(i, 0) * 100 / histMax)), Scalar(0));
+		cerr << "Image load failed!" << endl;
+		return -1;
 	}
-	return imgHist;
-}
 
-//상단 코드는 예제 5-7, 5-8과 같음.
-void main() {
-	Mat src = imread("coins.png", IMREAD_GRAYSCALE);
-	if (src.empty())
-	{
-		cerr << "로드 실패" << endl;
-		return;
-	}
-	double gmin, gmax;
-	double endsin_min= 30, endsin_max=200;
-	minMaxLoc(src, &gmin, &gmax);
+	imshow("src1", src1);
+	imshow("src2", src2);
 
-	Mat basic_dst = (src - gmin) * 255 / (gmax - gmin);
-	Mat Ends_in_dst = (src - endsin_min) * 255 / (endsin_max - endsin_min);
-	Mat equalization;
+	Mat dst1, dst2, dst2_1, dst3_1, dst3_2, dst3_3, dst4, dst5, dst6;
 
-	equalizeHist(src, equalization);
+	add(src1, src2, dst1);
+	dst2_1 = src1 - src2;
+	subtract(src1, src2, dst2);
+	addWeighted(src1, 0.1, src2, 0.1, 0, dst3_1);
+	addWeighted(src1, 0.5, src2, 0.5, 0, dst3_2);
+	addWeighted(src1, 1, src2, 1, 0, dst3_3);
+	absdiff(src1, src2, dst4);
+	multiply(src1, src2, dst5);
+	divide(src1, src2, dst6);
 
-	imshow("src", src);
-	imshow("src_Hist", getGrayHistImage(calcGrayHist(src)));
+	imshow("add", dst1);
+	imshow("subtract", dst2);
+	imshow("sub연산자 재정의", dst2_1);
+	imshow("addWeighted0.1", dst3_1);
+	imshow("addWeighted0.5", dst3_2);
+	imshow("addWeighted1.0", dst3_3);
+	imshow("absdiff", dst4);
+	imshow("multiply", dst5);
+	imshow("divide", dst6);
 
-	imshow("basic_dst", basic_dst);
-	imshow("basic_Hist", getGrayHistImage(calcGrayHist(basic_dst)));
+	waitKey();
 
-	imshow("Ends_in_dst", Ends_in_dst);
-	imshow("Ends_in_Hist", getGrayHistImage(calcGrayHist(Ends_in_dst)));
-
-	imshow("equalization", equalization);
-	imshow("equalization_Hist", getGrayHistImage(calcGrayHist(equalization)));
-
-	waitKey(0);
-	destroyAllWindows();
+	return 0;
 }
 
 
