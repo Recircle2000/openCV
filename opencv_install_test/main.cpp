b136f6b912c7f374767eb6d3f54710259e995ae8
Lee jaewon <ijw1004j@gmail.com>
2023-11-08 12:07:14 +0900

    엠보싱 필터링

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -7,35 +7,25 @@ using namespace std;
 
 int main(void)
 {
-	Mat src1 = imread("lenna256.bmp", IMREAD_GRAYSCALE);
-	Mat src2 = imread("square.bmp", IMREAD_GRAYSCALE);
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
 
-	if (src1.empty() || src2.empty())
+	if (src.empty())
 	{
-		cerr << "Image load failed!" << endl;
-		return -1;
+		cerr << " Images not found" << endl;
+		return 0;
 	}
 
-	imshow("src1", src1);
-	imshow("src2", src2);
+	float data[] = { -1,-1,0,-1,-1,1,0,1,1};
+	Mat emboss(3, 3, CV_32FC1, data);
 
-	Mat dst1, dst2, dst3, dst4;
+	Mat dst;
+	filter2D(src, dst, -1, emboss, Point(-1, -1),128);
 
-	bitwise_and(src1, src2, dst1);
-	bitwise_or(src1, src2, dst2);
-	bitwise_xor(src1, src2, dst3);
-	bitwise_not(src1, dst4);
+	imshow("src", src);
+	imshow("dst", dst);
 
-
-	imshow("Dst1", dst1);
-	imshow("Dst2", dst2);
-	imshow("Dst3", dst3);
-	imshow("Dst4", dst4);
-
-
-
-	waitKey();
-
+	waitKey(0);
+	destroyAllWindows();
 	return 0;
 }
 
