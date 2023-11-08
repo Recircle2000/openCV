2a382465a4e70959e04e992eb26f346792f9ec79
Lee jaewon <ijw1004j@gmail.com>
2023-11-08 12:07:14 +0900

    엠보싱 필터링

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -7,34 +7,19 @@ using namespace std;
 
 int main(void)
 {
-	Mat src1 = imread("lenna256.bmp", IMREAD_GRAYSCALE);
-	Mat src2 = imread("square.bmp", IMREAD_GRAYSCALE);
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
-	if (src1.empty() || src2.empty())
-	{
-		cerr << "Image load failed!" << endl;
-		return -1;
-	}
+	Mat dst1, dst2;
 
-	imshow("src1", src1);
-	imshow("src2", src2);
+	Canny(src, dst1, 50, 100);
+	Canny(src, dst2, 50, 300);
 
-	Mat dst1, dst2, dst3, dst4;
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
 
-	bitwise_and(src1, src2, dst1);
-	bitwise_or(src1, src2, dst2);
-	bitwise_xor(src1, src2, dst3);
-	bitwise_not(src1, dst4);
-
-
-	imshow("Dst1", dst1);
-	imshow("Dst2", dst2);
-	imshow("Dst3", dst3);
-	imshow("Dst4", dst4);
-
-
-
-	waitKey();
+	waitKey(0);
+	destroyAllWindows();
 
 	return 0;
 }
