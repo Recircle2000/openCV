2c968369409c749ab58dcf759a7fd16aa5ee64c7
Lee jaewon <ijw1004j@gmail.com>
2023-12-05 23:39:10 +0900

    미디언 필터링

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -15,25 +15,27 @@ int main(void)
 		return 0;
 	}
 
-	imshow("src", src);
+	//src 영상에서 10%에 해당하는 픽셀 값을 0 또는 255로 설정한다.
+	int num = (int)(src.total() * 0.1);
+	for (int i = 0; i < num; i++)
+	{
+		int x = rand() % src.cols;
+		int y = rand() % src.rows;
+		src.at<uchar>(y, x) = (i % 2) * 255;
+	}
 
-	Mat noise(src.size(), CV_32SC1);
-	randn(noise, 0, 5);
-	add(src, noise, src, Mat(),CV_8U);
-
+	//표준 편차가 인 가우시안 필터링을 수행하여 dst1에 저장
 	Mat dst1;
-	GaussianBlur(src, dst1, Size(), 5);
-
+	GaussianBlur(src, dst1, Size(), 1);
+	//크기가 3인 미디언 필터를 실행하여 dst2에 저장.
 	Mat dst2;
-	bilateralFilter(src, dst2, -1, 10, 5);
+	medianBlur(src, dst2, 3);
 
-	imshow("src" ,src);
-	imshow("dst1", dst1);
+	imshow("src", src);
+	imshow("dst", dst1);
 	imshow("dst2", dst2);
 
-	waitKey(0);
-	destroyAllWindows();
-
+	waitKey();
 	destroyAllWindows();
 	return 0;
 }
