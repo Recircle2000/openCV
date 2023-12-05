f884b90e9fdeaec25a30c0fe1a7296bdf1d7767c
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:47:50 +0900

    캐니 에지 검출

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -14,24 +14,16 @@ int main()
 		cerr << " Images not found" << endl;
 		return -1;
 	}
-	//x축 방향으로 1차 편미분, y축 방향으로 1차 편미분을 각각 구하여 dx와 dy행렬에 저장. dx와 dy행렬을 float자료형을 사용하도록 설정.
-	Mat dx, dy;
-	Sobel(src, dx, CV_32FC1, 1, 0);
-	Sobel(src, dy, CV_32FC1, 0, 1);
 
-	Mat fmag, mag;
-	//dx와 dy행렬로부터 그래디언트 크기를 계산하여 fmag에 저장한다. dx와dy가 모두 float 자료형을 사용하므로 fmag도 flaot 자료형을 사용하는 행렬로 생성.
-	magnitude(dx, dy, fmag);
+	Mat dst1, dst2;
+	//낮은 임계값을 50, 높은 임계값을 100으로 설정하여 케니 에지 검출 수행.
+	Canny(src, dst1, 50, 100);
 
-	//실수ㅕㅇ 행렬 famg를 그레이스케일 형식으로 변환하여 mag에 저장.
-	fmag.convertTo(mag, CV_8UC1);
-	//엣지 판별을 위한 그래디언트 크기 임계값을 150으로 설정하여 에지를 판별, 행렬 dege의 우너소값은 mag행렬 원소 갑싱 150보다 크면 255로, 작으면 0으로 설정.
-	Mat edge = mag > 150;
-
+	Canny(src, dst2, 50, 150);
 
 	imshow("src", src);
-	imshow("edge", edge);
-	imshow("mag", mag);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
 
 	waitKey(0);
 	
