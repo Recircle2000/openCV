92b3483637c951a34574c11f6ab89e096bcef388
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:09:45 +0900

    영상의 이동 변환

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -14,27 +14,15 @@ int main(void)
 		cerr << " Images not found" << endl;
 		return 0;
 	}
-	//입력 영상과 출력 영상에서의 세 점 좌표를 저장할 srcPts와 dstPts 배열을 선언한다.
-	Point2f srcPts[3], dstPts[3];
-	//srcPts 배열에 입력 영상의 좌측 상단, 우측상단, 우측 하단의 좌표를 저장한다.
-	srcPts[0] = Point2f(0, 0);
-	srcPts[1] = Point2f(src.cols - 1, 0);
-	srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
-	//dstPts배열에 srcPts 점들이 이동할 좌표를 저장한다.
-	dstPts[0] = Point2f(50, 50);
-	dstPts[1] = Point2f(src.cols - 100, 100);
-	dstPts[2] = Point2f(src.cols - 50, src.rows - 50);
 
-	//2x3 어파인 변환 행렬을 m에 저장한다.
-	Mat m = getAffineTransform(srcPts, dstPts);
+	//가로를 150픽셀, 세로로100픽셀 이동하는 어파인 변환 행렬를 생성,
+	Mat m = Mat_<double>({ 2,3 }, { 1,0,150,0,1,100 });
 
-	//어파인 변환 행렬m을 이용하여 src영상을 어파인 변환하여 dst에 저장한다. warpAffine()함수의 네번재 인자에 Size()를 지정하여 dst영상 크기가 src영상 크기와 같아지도록 설정하였다.
 	Mat dst;
 	warpAffine(src, dst, m, Size());
 
 	imshow("src", src);
 	imshow("dst", dst);
-	
 
 	waitKey();
 	destroyAllWindows();
