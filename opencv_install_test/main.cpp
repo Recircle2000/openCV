849a05f30ff91c5100918ca368fbcf47c1aa004a
Recircle2000 <ijw1004j@gmail.com>
2023-12-05 23:25:14 +0900

    양방향 필터링

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -17,22 +17,21 @@ int main(void)
 
 	imshow("src", src);
 
-	for (int stddev = 0; stddev <= 30; stddev += 10)
-	{
-		//평균이 0이고, 표준 편차가 stddev인 가우시안 잡읍을 생성하여 noise 행렬에 저장. 이때 noist 행렬을 부호 있는 정수형(32SC1)을 사용하도록 미리 생성하여 randn함수에 전달.
-		Mat noise(src.size(), CV_32SC1);
-		randn(noise, 0, stddev);
+	Mat noise(src.size(), CV_32SC1);
+	randn(noise, 0, 5);
+	add(src, noise, src, Mat(),CV_8U);
 
-		Mat dst;
-		//입력 영상 src에 가우시안 잡음 noise를 더하여 결과 영상 dst를 생성합니다. dst영상의 깊이는 8U로 설정한다.
-		add(src, noise, dst, Mat(), CV_8U);
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 5);
 
-		String desc = format("stddev = %d", stddev);
-		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+	Mat dst2;
+	bilateralFilter(src, dst2, -1, 10, 5);
 
-		imshow("dst", dst);
-		waitKey();
-	}
+	imshow("src" ,src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+
+	waitKey();
 
 	destroyAllWindows();
 	return 0;
