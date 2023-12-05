203cdf58e1c5742178de44bdd42143cdcb021424
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:45:57 +0900

    (엣지검출) 소벨 마스크 기반 에지 검출 예제

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -5,62 +5,38 @@
 using namespace cv;
 using namespace std;
 
-Mat src;
-Point2f srcQuad[4], dstQuad[4];
-
-void on_mouse(int event, int x, int y, int flags, void* userdata);
 int main()
 {
-	src = imread("card.bmp");
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
 	if (src.empty())
 	{
 		cerr << " Images not found" << endl;
 		return -1;
 	}
+	//x축 방향으로 1차 편미분, y축 방향으로 1차 편미분을 각각 구하여 dx와 dy행렬에 저장. dx와 dy행렬을 float자료형을 사용하도록 설정.
+	Mat dx, dy;
+	Sobel(src, dx, CV_32FC1, 1, 0);
+	Sobel(src, dy, CV_32FC1, 0, 1);
 
-	namedWindow("src");
-	setMouseCallback("src", on_mouse);
+	Mat fmag, mag;
+	//dx와 dy행렬로부터 그래디언트 크기를 계산하여 fmag에 저장한다. dx와dy가 모두 float 자료형을 사용하므로 fmag도 flaot 자료형을 사용하는 행렬로 생성.
+	magnitude(dx, dy, fmag);
+
+	//실수ㅕㅇ 행렬 famg를 그레이스케일 형식으로 변환하여 mag에 저장.
+	fmag.convertTo(mag, CV_8UC1);
+	//엣지 판별을 위한 그래디언트 크기 임계값을 150으로 설정하여 에지를 판별, 행렬 dege의 우너소값은 mag행렬 원소 갑싱 150보다 크면 255로, 작으면 0으로 설정.
+	Mat edge = mag > 150;
+
 
 	imshow("src", src);
+	imshow("edge", edge);
+	imshow("mag", mag);
+
 	waitKey(0);
 	
 	destroyAllWindows();
 	return 0;
-}
-
-void on_mouse(int event, int x, int y, int flags, void*)
-{
-	static int cnt = 0;
-
-	if (event == EVENT_LBUTTONDOWN)
-	{
-		if (cnt < 4)
-		{
-			srcQuad[cnt++] = Point2f(x, y);
-
-			circle(src, Point(x, y), 5, Scalar(0, 0, 255), -1);
-			imshow("src", src);
-
-			if (cnt == 4)
-			{
-				int w = 200, h = 300;
-
-				dstQuad[0] = Point2f(0, 0);
-				dstQuad[1] = Point2f(w - 1, 0);
-				dstQuad[2] = Point2f(w - 1, h - 1);
-				dstQuad[3] = Point2f(0, h - 1);
-
-				Mat pers = getPerspectiveTransform(srcQuad, dstQuad);
-
-				Mat dst;
-				warpPerspective(src, dst, pers, Size(w, h));
-
-				imshow("dst", dst);
-
-			}
-		}
-	}
 }
 
 
