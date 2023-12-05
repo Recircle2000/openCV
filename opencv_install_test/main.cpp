4ce2d686ae39b4448eb1d71239d347f1bd51e395
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 01:05:47 +0900

    확률적 허프 변환 선분 검출

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -14,36 +14,24 @@ int main()
 		cerr << " Images not found" << endl;
 		return -1;
 	}
-
-	//캐니 에지 검출기를 이용하여 구한 에지 영상을 edge에 저장.
 	Mat edge;
 	Canny(src, edge, 50, 150);
 
-	//HoughLines() 함수를 이용하여 직선의 방정식 파라미터 p와 세타 정보를 lines에 저장한다. 축적 배열에서 p 간격은 1픽셀 단위로 세타는 1도 단위로 처리
-	vector<Vec2f> lines;
-	HoughLines(edge, lines, 1, CV_PI / 180, 250);
+	//HoughLinesP()함수를 이용하여 모든 직선 성분의 시작점과 끝점 좌표를 구합니다.
+	vector<Vec4i> lines;
+	HoughLinesP(edge, lines, 1, CV_PI / 180, 160,50,5);
 
 	Mat dst;
 	cvtColor(edge, dst, COLOR_GRAY2BGR);
 
-	//HoughLins()함수에 의해 구해진 직선의 개수 만큼 for반복문을 수행한다.
-	for (size_t i = 0; i < lines.size(); i++)
+	//HoughLinesP() 함수에 의해 구해진 모든 직선 성분을 dst 영상 위에 빨간색 직선으로 그린다.
+	for(Vec4i l : lines)
 	{
-		float r = lines[i][0], t = lines[i][1];
-		//직선의 방정식 파라미터 중에서 p를 변수r에, 세타를 변수t에 저장. 
-		double cos_t = cos(t), sin_t = sin(t);
-		//x0,y0은 원점에서 직선에 수선을 내렷을 때 만나는 점의 좌표.
-		double x0 = r * cos_t, y0 = r * sin_t;
-		double alpla = 1000;
-		//pt1,pt2에는 (x0,y0)에서 충분히 멀리 떨어져 있는는 직선상의 두 점 좌표가 저장된다.
-		Point pt1(cvRound(x0 + alpla * (-sin_t)), cvRound(y0 + alpla * cos_t));
-		Point pt2(cvRound(x0 - alpla * (-sin_t)), cvRound(y0 - alpla * cos_t));
-		line(dst, pt1, pt2, Scalar(0, 0, 255), 2, LINE_AA);
-
+		line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, LINE_AA);
 	}
 
 	imshow("src", src);
-	imshow("dst1", dst);
+	imshow("dst", dst);
 
 	waitKey(0);
 	
