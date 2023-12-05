54fb78d5a3fea06539b9b05c29fb3edaad4fa863
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:11:49 +0900

    영상의 전단 변환

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -15,11 +15,14 @@ int main(void)
 		return 0;
 	}
 
-	//가로를 150픽셀, 세로로100픽셀 이동하는 어파인 변환 행렬를 생성,
-	Mat m = Mat_<double>({ 2,3 }, { 1,0,150,0,1,100 });
+
+	//가로 방향으로 밀림 정도를 0.3으로 설정한 전단 변활 행렬m을 생성
+	double mx = 0.3;
+	Mat m = Mat_<double>({ 2,3 }, { 1,mx,0,0,1,0 });
 
 	Mat dst;
-	warpAffine(src, dst, m, Size());
+	//행렬 m을 이용하여 어파인 변환 수행, 전단 변환에 의해 입력 영상의 일부가 잘리지 않도록 결과 영상 가로 크기를 cvRound()를 형태로 지정.
+	warpAffine(src, dst, m, Size(cvRound(src.cols + src.rows * mx), src.rows));
 
 	imshow("src", src);
 	imshow("dst", dst);
