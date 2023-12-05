007819a444272e8b6499158d1ece127319c9bc6d
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:23:54 +0900

    영상의 대칭 변환

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -15,18 +15,24 @@ int main(void)
 		return 0;
 	}
 
-	//영상의 중심 좌표를 가티리는 변수 cp를 선언
-	Point2f cp(src.cols / 2.f, src.rows / 2.f);
-	//cp좌표를 기준으로 반시계 방향으로 20도 회전하는 변환행렬 생성.
-	Mat m = getRotationMatrix2D(cp, 20, 1);
-
-	//변환 행렬 m를 이용하여 src 영상을 어파인 변환하고, 그 결과를 dst에 저장한다.
-	Mat dst;
-	warpAffine(src, dst, m, Size());
-
 	imshow("src", src);
-	imshow("dst", dst);
-	waitKey();
+
+	Mat dst;
+	//flip()함수에 전달할 flipCOde 세 개를 정수형 배열에 저장.
+	//양수 : 좌우대칭, 0 : 상하대칭, -1 : 상하, 좌우 모두
+	int flipCode[] = {1, 0, -1};
+	for (int i = 0; i < 3; ++i)
+	{
+		//flipCode 배열에 저장된 정수 값을 이용하여 대칭 변활을 수행한다.
+		flip(src, dst, flipCode[i]);
+
+		String desc = format("flopCOde: %d", flipCode[i]);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 1, LINE_AA);
+
+		imshow("dst", dst);
+		waitKey();
+	}
+	
 	destroyAllWindows();
 	return 0;
 }
