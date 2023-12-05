dd3b1798e3fa9e0414b2ed4796b210803685f211
Recircle2000 <ijw1004j@gmail.com>
2023-12-05 23:21:29 +0900

    가우시안 잡음 추가

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(void)
 {
-	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
 	if (src.empty())
 	{
@@ -16,25 +16,22 @@ int main(void)
 	}
 
 	imshow("src", src);
-	
-	//sigma 값을 1부터 5까지 증가시키면서 언샤프 마스크 필터링을 수행하고 그 결과를 화면에 출력.
-	for (int sigma = 1; sigma <=5; sigma ++)
+
+	for (int stddev = 0; stddev <= 30; stddev += 10)
 	{
-		//가우시안 필터를 이용한 블러링 영상을 plurred에 저장
-		Mat blurred;
-		GaussianBlur(src, blurred, Size(), sigma);
+		//평균이 0이고, 표준 편차가 stddev인 가우시안 잡읍을 생성하여 noise 행렬에 저장. 이때 noist 행렬을 부호 있는 정수형(32SC1)을 사용하도록 미리 생성하여 randn함수에 전달.
+		Mat noise(src.size(), CV_32SC1);
+		randn(noise, 0, stddev);
 
-		//언샤프 마스크 필터링 수행
-		float alpha = 1.f;
-		Mat dst = (1 + alpha) * src - alpha * blurred;
-		//샤프닝 결과 영상 dst에 사용된 sigma값을 출력.
-		String dest = format("sigma = %d", sigma);
-		putText(dst, dest, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0,
-			Scalar(255), 1, LINE_AA);
+		Mat dst;
+		//입력 영상 src에 가우시안 잡음 noise를 더하여 결과 영상 dst를 생성합니다. dst영상의 깊이는 8U로 설정한다.
+		add(src, noise, dst, Mat(), CV_8U);
 
+		String desc = format("stddev = %d", stddev);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
 
 		imshow("dst", dst);
-		waitKey(0);
+		waitKey();
 	}
 
 	destroyAllWindows();
