a76b16f3bd4046efaf930528ff75df6a5ac52bff
Lee jaewon <ijw1004j@gmail.com>
2023-12-06 00:20:04 +0900

    영상의 회전 변환

--- opencv_install_test/main.cpp
+++ opencv_install_test/main.cpp
@@ -15,24 +15,17 @@ int main(void)
 		return 0;
 	}
 
+	//영상의 중심 좌표를 가티리는 변수 cp를 선언
+	Point2f cp(src.cols / 2.f, src.rows / 2.f);
+	//cp좌표를 기준으로 반시계 방향으로 20도 회전하는 변환행렬 생성.
+	Mat m = getRotationMatrix2D(cp, 20, 1);
 
-	Mat dst1, dst2, dst3, dst4;
-	//src영상을 x방향으로 4배, y방향으로 4배 확대하여 dst1을 생성한다. src영상의 크기가 480x320이므로 결과영상은 FHD로 결정. 보간법은 최근방 이웃 보간법.
-	resize(src, dst1, Size(), 4, 4, INTER_NEAREST);
-	//src 영상을 FHD크기로 확대하여 dst2를 생성. 보간법을 지정하지 않아서 기본값인 양선형 보간법 사용.
-	resize(src, dst2, Size(1920, 1080));
-	//3차 회선 보간법 사용.
-	resize(src, dst3, Size(1920, 1080), 0, 0, INTER_CUBIC);
-	//란초스 보간법 사용
-	resize(src, dst4, Size(1920, 1080), 0, 0, INTER_LANCZOS4);
+	//변환 행렬 m를 이용하여 src 영상을 어파인 변환하고, 그 결과를 dst에 저장한다.
+	Mat dst;
+	warpAffine(src, dst, m, Size());
 
-	//400,500 좌표부터 400,400만큼의 부분 영상 출력
 	imshow("src", src);
-	imshow("dst", dst1(Rect(400,500,400,400)));
-	imshow("dst2", dst2(Rect(400, 500, 400, 400)));
-	imshow("dst3", dst3(Rect(400, 500, 400, 400)));
-	imshow("dst4", dst4(Rect(400, 500, 400, 400)));
-
+	imshow("dst", dst);
 	waitKey();
 	destroyAllWindows();
 	return 0;
